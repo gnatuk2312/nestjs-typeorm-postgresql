2b077c391e6c54ef268b56335194c247e5b26185
Hnat Liashenko <hnat.liashenko@bluepes.com>
2024-03-15 20:23:34 +0200

    implement dto validation using class-validator library

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(new ValidationPipe());
 
   const configService = app.get(ConfigService);
 
--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -1,7 +1,18 @@
+import { IsEmail, IsPhoneNumber, IsString } from 'class-validator';
+
 export class CreateUserDTO {
+  @IsString()
   firstName: string;
+
+  @IsString()
   lastName: string;
+
+  @IsEmail()
   email: string;
+
+  @IsPhoneNumber()
   phoneNumber: string;
+
+  @IsString()
   password: string;
 }
